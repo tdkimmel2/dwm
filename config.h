d72900a2d897bf6fb4e2086aaa6555c0ce8fbb31
tdkimmel2 <tdkimmel2@gmail.com>
2022-11-14 19:26:58 -0500

    Changed out dwm-bar for dwmblocks (now in it's own repo). Revoked the extrabar patch and put in the statuscmd patch for extra functionality with dwmblocks

--- config.h
+++ config.h
@@ -10,9 +10,9 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char statussep         = ';';      /* separator between status bars */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+//static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "noto:size=15" };
+static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -65,13 +65,14 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#define STATUSBAR "dwmblocks"
+
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *volupcmd[] = { "/bin/sh", "-c", "amixer -D default sset Master Playback 5%+" };
-static const char *voldowncmd[] = { "/bin/sh", "-c", "amixer -D default sset Master Playback 5%-" };
-static const char *volmutecmd[] = { "/bin/sh", "-c", "amixer -D default sset Master Playback 0%" };
+static const char *volupcmd[] = { "/bin/sh", "-c", "amixer -D default sset Master Playback 5%+; kill -44 $(pidof dwmblocks)" };
+static const char *voldowncmd[] = { "/bin/sh", "-c", "amixer -D default sset Master Playback 5%-; kill -44 $(pidof dwmblocks)" };
+static const char *volmutecmd[] = { "/bin/sh", "-c", "amixer -D default sset Master Playback 0%; kill -44 $(pidof dwmblocks)" };
 //static const char *volmaxcmd[] = { "/bin/sh", "-c", "amixer -D default sset Master Playback 100%" };
 
 static const Key keys[] = {
@@ -138,7 +139,9 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
